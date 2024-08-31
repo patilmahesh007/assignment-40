0b52b065929dea9c950bd03c6ad67d773a7e59a9
patilmahesh007 <patilmahesh4007>
2024-08-31 13:17:11 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './view/App/App';
-import Contact from './view/Contact/contact';
-import About from './view/About-us/about';
+
+
+import Home from "../src/view/App/App";
+import About from  "../src/view/About-us/about"
+import Contact from "../src/view/Contact/contact"
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
-);
+const currentPath = window.location.pathname;
+if (currentPath == "/") {
+  root.render(<Home />)
+}
+else if (currentPath == "/about") {
+  root.render(<About />)
+}
+else if (currentPath == "/contact") {
+  root.render(<Contact />)
+}
+else {
+  root.render(<h1>404</h1>)
+}
